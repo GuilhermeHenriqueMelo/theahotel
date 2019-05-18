2e4426fdd78100829ae0db98601b400ecd2c9b38
GuilhermeHenriqueMelo <guilherme.melo0500@gmail.com>
2019-05-18 14:18:54 -0300

    Added more components to the project and started to fill them with content

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,27 @@
 import {MatBadgeModule} from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     imports: [MatBadgeModule,
-              MatButtonModule],
+              MatButtonModule,
+              MatSidenavModule,
+              MatToolbarModule,
+              MatIconModule,
+              MatListModule,
+              MatTabsModule],
     exports: [MatBadgeModule,
-              MatButtonModule]
+              MatButtonModule,
+              MatSidenavModule,
+              MatToolbarModule,
+              MatIconModule,
+              MatListModule,
+              MatTabsModule]
 })
 
 export class MaterialModule {}
